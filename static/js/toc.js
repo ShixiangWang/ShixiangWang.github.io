ceb6e3f4b2ce328b62f7fd9a65f39243b06eed90
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2021-08-06 19:52:33 +0800

    Fix toc navigation
    只支持 H2 标题

--- static/js/toc.js
+++ static/js/toc.js
@@ -1,15 +1,40 @@
+// Cannot extract h3 headings
+
 document.addEventListener('DOMContentLoaded', function() {
   function createTocList (el, selectors) {
-    selectors = selectors || ['.section.level2', '.section.level3']
+    if (typeof(selectors) == "undefined") {
+      if (el.querySelectorAll('.section.level2').length < 1) {
+        if (el.querySelectorAll('h2').length < 1) {
+         return '' 
+        }
+        selectors =  ['h2', 'h3'] // 'h2, h3'
+      } else {
+        selectors = ['.section.level2', '.section.level3']
+      }
+    } //else {
+      //console.log("This is the second time")
+      //console.log(selectors[0])
+      //console.log(el.querySelectorAll("h2, h3").length)
+    //}
+    
     if (!Array.isArray(selectors)) {
       selectors = [selectors]
     }
+    
     var els = el.querySelectorAll(selectors[0])
-    if (els.length < 1) return ''
+    if (els.length < 1) {
+      return ''
+    }
+    
     var toc = `<ul>`
     els.forEach(function(el) {
-      toc += `<li><a href="#${el.id}">${el.firstElementChild.textContent}</a>`
+      if (selectors[0].substring(0, 1) == 'h') {
+        toc += `<li><a href="#${el.id}">${el.textContent}</a>`
+      } else {
+        toc += `<li><a href="#${el.id}">${el.firstElementChild.textContent}</a>`
+      }
       if (selectors.length > 1) {
+        //console.log("Run again")
         toc += createTocList(el, selectors.slice(1))
       }
       toc += '</li>'
