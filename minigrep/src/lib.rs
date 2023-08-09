6aaa5942cf01cd9a8302cb344adc67f45148e208
Shixiang Wang (王诗翔) <wang.shixiang@icloud.com>
2023-08-09 22:16:07 +0800

    Update lib.rs

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,15 +16,22 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path})
+        Ok(Config { query, file_path, ignore_case})
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
     
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insenstive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -39,6 +48,22 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         }
     }
     
+    results
+}
+
+pub fn search_case_insenstive<'a>(
+    query: &str,
+    contents: &'a str,
+    ) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
     results
 }
 
@@ -62,5 +87,20 @@ safe, fast, productive.
 Pick three.";
         
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insenstive(query, contents)
+            );
     }
 }
