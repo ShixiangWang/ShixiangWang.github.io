2dc721ad8e080acf5c81ad82c29ebe15f6947b1b
Shixiang Wang (王诗翔) <wang.shixiang@icloud.com>
2023-08-28 23:41:51 +0800

    finish minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,10 +4,7 @@ use std::process;
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-//    let config = parse_config(&args);
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
