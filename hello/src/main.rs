d2910f9da0dd8e56cf5eae347f4f9d6557d84c0f
Shixiang Wang (王诗翔) <wang.shixiang@icloud.com>
2023-08-31 23:55:00 +0800

    up up

---
+++ hello/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("Hello, world!");
+    
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+}
